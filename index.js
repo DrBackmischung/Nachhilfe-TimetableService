2b1b2ad021f9d4da3e2921568c942e24d4f5eeaf
Soufiane461 <79450257+Soufiane461@users.noreply.github.com>
2022-12-11 11:46:46 +0100

    all

--- index.js
+++ index.js
@@ -25,52 +25,11 @@ const app = express();
 
 app.use(express.json());
 
-const thisWillRunInEveryRequest = (req, res, next) => {
-  console.log('running the middleware for', req.method, req.originalUrl);
-  next();
-}
-
-app.use(thisWillRunInEveryRequest);
-
 
 app.get('/timeslots', async(req, res) => {
   const timeslots = await Timeslot.findAll();
   res.send(timeslots);
 })
-
-/*app.get('timeslots', (req, res) => {
-  let fach = req.query.id
-  if(timeslots [fach]){
-    res.send(timeslots)
-  }else{
-    res.send('not found')
-  }
-})
-*/
-
-app.post('/timeslots', async (req, res) => {
-  await Timeslot.create(req.body);
-  res.send("success");
-})
-
-
-function InvalidIdException(){
-  this.status = 400;
-  this.message = 'Invalid ID';
-}
-
-function TimeslotNotFoundException(){
-  this.status = 404;
-  this.message = 'Timeslot not found';
-}
-
-const idNumberControl = (req, res, next) => {
-  const id = Number.parseInt(req.params.id);
-  if (Number.isNaN(id)) {
-    throw new InvalidIdException();
-  }
-  next();
-}
 
 app.get('/timeslots/:id', idNumberControl, async (req, res, next) => {
   const id = req.params.id;
@@ -82,12 +41,42 @@ app.get('/timeslots/:id', idNumberControl, async (req, res, next) => {
 })
 
 app.get('/timeslots/schueler/:schuelerId', idNumberControl, async (req, res, next) => {
-  
+  const schuelerId = req.params.schuelerId;
+  const timeslot = await Timeslot.findAll({where: {schuelerId: schuelerId}});
+  if(!timeslot) {
+    next(new TimeslotNotFoundException());
+  }
+  res.send(timeslot);
 })
 
-app.get('/timeslots/lehrer/:schuelerId', idNumberControl, async (req, res, next) => {
-  
+app.get('/timeslots/lehrer/:lehrerId', idNumberControl, async (req, res, next) => {
+  const lehrerId = req.params.lehrerId;
+  const timeslot = await Timeslot.findAll({where: {lehrerId: lehrerId}});
+  if(!timeslot) {
+    next(new TimeslotNotFoundException());
+  }
+  res.send(timeslot);
 })
+
+
+app.get('/timeslots/matching/:lehrerId/:schuelerId', idNumberControl, async (req, res, next) => {
+  const lehrerId = req.params.lehrerId;
+  const schuelerId = req.params.schuelerId;
+  const timeslot = await Timeslot.findAll({
+    where: [{lehrerId: lehrerId}, {schuelerId: schuelerId}]
+  });
+  if(!timeslot) {
+    next(new TimeslotNotFoundException());
+  }
+  res.send(timeslot);
+})
+
+app.post('/timeslots', async (req, res) => {
+  await Timeslot.create(req.body);
+  res.send("success");
+})
+
+
 
 app.put('/timeslots/:id', async (req, res) => {
   const requestedId = req.params.id;
@@ -113,6 +102,10 @@ app.delete('/timeslots/:id', idNumberControl, async (req, res) => {
   res.send('removed');
 })
 
+
+
+
+
 app.use((err, req, res, next) => {
   res
     .status(err.status)
@@ -123,6 +116,34 @@ app.use((err, req, res, next) => {
         path: req.originalUrl
       });
 })
+
+
+const thisWillRunInEveryRequest = (req, res, next) => {
+  console.log('running the middleware for', req.method, req.originalUrl);
+  next();
+}
+
+function InvalidIdException(){
+  this.status = 400;
+  this.message = 'Invalid ID';
+}
+
+function TimeslotNotFoundException(){
+  this.status = 404;
+  this.message = 'Timeslot not found';
+}
+
+
+const idNumberControl = (req, res, next) => {
+  if(req.params.id != null ){
+  const id = Number.parseInt(req.params.id);
+  if (Number.isNaN(id)) {
+    throw new InvalidIdException();
+  }}
+  next();
+}
+
+app.use(thisWillRunInEveryRequest);
 
 app.listen(3000, () => {
   console.log("app is running");
